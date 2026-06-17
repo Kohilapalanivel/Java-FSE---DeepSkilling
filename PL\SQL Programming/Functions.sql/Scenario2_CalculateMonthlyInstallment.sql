a39b465e5c572be2dd248a1c735a2ea902ca6a5f
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-17 20:26:02 +0530

    Create Scenario2_CalculateMonthlyInstallment.sql

---
+++ PL\SQL Programming/Functions.sql/Scenario2_CalculateMonthlyInstallment.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION CalculateMonthlyInstallment(
+    p_LoanAmount NUMBER,
+    p_InterestRate NUMBER,
+    p_Years NUMBER
+)
+RETURN NUMBER
+IS
+    v_TotalAmount NUMBER;
+    v_MonthlyInstallment NUMBER;
+BEGIN
+
+    v_TotalAmount :=
+    p_LoanAmount +
+    (p_LoanAmount * p_InterestRate * p_Years / 100);
+
+    v_MonthlyInstallment :=
+    v_TotalAmount / (p_Years * 12);
+
+    RETURN ROUND(v_MonthlyInstallment,2);
+
+END;
+/

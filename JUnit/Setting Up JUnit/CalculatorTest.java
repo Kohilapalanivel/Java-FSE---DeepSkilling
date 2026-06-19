03810c7760e69e237deefdaeca991c39e58b3267
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-19 11:56:34 +0530

    Refactor CalculatorTest formatting and structure

---
+++ JUnit/Setting Up JUnit/CalculatorTest.java
@@ -0,0 +1,12 @@
+package junitpractice;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+class CalculatorTest 
+{
+	@Test
+	void testAdd() 
+  {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2,3));
+    }
+}

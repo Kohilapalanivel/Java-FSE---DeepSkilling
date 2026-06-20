fb2b33d336f42d2072fd5ba26b6e71f69cbe9c5f
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-20 18:32:52 +0530

    Create CalculatorTest.java

---
+++ JUnit/Advanced JUnit Testing/Test Suites and Categories/CalculatorTest.java
@@ -0,0 +1,24 @@
+package junitpractice;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest 
+{
+    Calculator calc = new Calculator();
+    @Test
+    void testAdd() {
+        assertEquals(15, calc.add(10, 5));
+    }
+    @Test
+    void testSubtract() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+    @Test
+    void testMultiply() {
+        assertEquals(50, calc.multiply(10, 5));
+    }
+    @Test
+    void testDivide() {
+        assertEquals(2, calc.divide(10, 5));
+    }
+}

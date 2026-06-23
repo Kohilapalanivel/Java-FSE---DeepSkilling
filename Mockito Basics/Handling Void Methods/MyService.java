98ab10c7ec079914c9ce6b1a1def4a47dde62755
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-23 17:55:33 +0530

    Create MyService.java

---
+++ Mockito Basics/Handling Void Methods/MyService.java
@@ -0,0 +1,13 @@
+package mockitoPractice;
+public class MyService 
+{
+	private ExternalApi api;
+    public MyService(ExternalApi api) 
+    {
+        this.api = api;
+    }
+    public void processData() 
+    {
+      api.saveData("Hello Mockito");
+    }
+}

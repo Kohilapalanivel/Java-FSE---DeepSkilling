7c647c33154affdd1a6fb47b779f496045527654
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-23 18:25:40 +0530

    Add test for verifying method call order in MyService

---
+++ Mockito Basics/Verifying Interaction Order/MyServiceTest.java
@@ -0,0 +1,20 @@
+package mockitoPractice;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+public class MyServiceTest 
+{
+    @Test
+    public void testMethodCallOrder() 
+    {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.process();
+        InOrder inOrder = inOrder(mockApi);
+        inOrder.verify(mockApi).login();
+        inOrder.verify(mockApi).fetchData();
+        inOrder.verify(mockApi).logout();
+    }
+
+}

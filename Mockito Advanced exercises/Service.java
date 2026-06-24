ed02bc4dbf19823409726f4021ed48955c3cfea6
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-24 19:45:33 +0530

    Create Service.java

---
+++ Mockito Advanced exercises/Service.java
@@ -0,0 +1,13 @@
+package mockitoPractice;
+public class Service 
+{
+	private Repository repo;
+    public Service(Repository repo) 
+    {
+        this.repo = repo;
+    }
+    public String processData() 
+    {
+        return "Processed " + repo.getData();
+    }
+}

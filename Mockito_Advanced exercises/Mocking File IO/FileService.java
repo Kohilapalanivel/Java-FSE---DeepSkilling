db55eca831e23cac7b748466f9abb4c3bbcbc049
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-24 20:39:31 +0530

    Create FileService.java

---
+++ Mockito_Advanced exercises/Mocking File IO/FileService.java
@@ -0,0 +1,16 @@
+package mockitoPractice;
+public class FileService {
+	private FileReader reader;
+	private FileWriter writer;
+	public FileService(FileReader reader,FileWriter writer)
+	{
+		this.reader = reader;
+		this.writer = writer;
+	}
+	public String processFile()
+	{
+        String content = reader.read();
+        writer.write(content);
+        return "Processed " + content;
+    }
+}

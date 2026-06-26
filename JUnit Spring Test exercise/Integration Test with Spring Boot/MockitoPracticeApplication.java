9a6e5124d94291c3652d0579efc3b01082818e9b
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-26 20:42:49 +0530

    Create MockitoPracticeApplication.java

---
+++ JUnit Spring Test exercise/Integration Test with Spring Boot/MockitoPracticeApplication.java
@@ -0,0 +1,12 @@
+package mockitoPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoPracticeApplication.class, args);
+    }
+}

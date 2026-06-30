49a32753d382749ba29933022bf36becd97062ce
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-06-30 22:53:48 +0530

    Create CountryService.java

---
+++ Spring Data JPA with Hibernate/1. spring-data-jpa-handson/Add a new country/orm-learn/com.cognizant.ormlearn/service/CountryService.java
@@ -0,0 +1,36 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.exception.CountryNotFoundException;
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode)
+            throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found");
+        }
+
+        return result.get();
+    }
+}

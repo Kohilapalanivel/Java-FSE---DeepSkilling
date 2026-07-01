a1ff654f3bc184e8374518639728763359199658
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 23:17:48 +0530

    Create AttemptRepository.java

---
+++ Spring Data JPA with Hibernate/3. spring-data-jpa-handson/3.Fetch quiz attempt/orm-learn/repository/AttemptRepository.java
@@ -0,0 +1,21 @@
+package com.cognizant.ormlearn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cognizant.ormlearn.model.Attempt;
+
+public interface AttemptRepository extends JpaRepository<Attempt, Integer> {
+
+	@Query("""
+			SELECT a FROM Attempt a
+			JOIN FETCH a.user u
+			JOIN FETCH a.attemptQuestions aq
+			JOIN FETCH aq.question q
+			JOIN FETCH aq.attemptOptions ao
+			JOIN FETCH ao.option
+			WHERE u.id = :userId AND a.id = :attemptId
+			""")
+			Attempt getAttemptDetails(int userId, int attemptId);
+}

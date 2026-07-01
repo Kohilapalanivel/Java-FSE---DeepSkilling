3bce9e4cf6f0959e067051ffe76900c65fe40db2
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 11:26:59 +0530

    Create skill.sql

---
+++ Spring Data JPA with Hibernate/2. spring-data-jpa-handson/3.Payrolls tables and bean mapping/Table Creation and Value Insertion/skill.sql
@@ -0,0 +1,5 @@
+CREATE TABLE skill (
+    sk_id INT AUTO_INCREMENT PRIMARY KEY,
+    sk_name VARCHAR(100) NOT NULL
+);
+INSERT INTO skill(sk_name) VALUES ('Java'),('Spring Boot'),('SQL'),('Hibernate');

a4446b4498a705d6fc3e0f2c139bfd92d81ab066
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 23:37:36 +0530

    Create EmployeeRepository.java

---
+++ Spring Data JPA with Hibernate/3. spring-data-jpa-handson/5.Get all employees using Native Query/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package com.cognizant.ormlearn.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cognizant.ormlearn.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    Employee findById(int id);
+
+    @Query("SELECT e FROM Employee e left join fetch e.department left join fetch e.skillList WHERE e.permanent = true")
+    List<Employee> getAllPermanentEmployees();
+
+    @Query("SELECT AVG(e.salary) FROM Employee e WHERE e.department.id = :id")
+    double getAverageSalary(@Param("id") int id);
+
+    @Query(value = "SELECT * FROM employee", nativeQuery = true)
+    List<Employee> getAllEmployeesNative();
+}

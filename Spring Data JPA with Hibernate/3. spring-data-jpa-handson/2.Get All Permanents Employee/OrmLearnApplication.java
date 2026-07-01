b9367e3035104021e74d48cb7566608f84cdcd1c
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 19:56:07 +0530

    Create OrmLearnApplication.java

---
+++ Spring Data JPA with Hibernate/3. spring-data-jpa-handson/2.Get All Permanents Employee/OrmLearnApplication.java
@@ -0,0 +1,33 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Employee;
+import com.cognizant.ormlearn.service.EmployeeService;
+
+@SpringBootApplication
+public class OrmLearnApplication 
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    private static EmployeeService employeeService;
+    public static void main(String[] args) 
+    {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        employeeService = context.getBean(EmployeeService.class);
+        testGetAllPermanentEmployees();
+    }
+    private static void testGetAllPermanentEmployees() 
+    {
+        LOGGER.info("Start");
+        List<Employee> employees = employeeService.getAllPermanentEmployees();
+        LOGGER.debug("Permanent Employees : {}", employees);
+        employees.forEach(employee -> LOGGER.debug("Skills : {}", employee.getSkillList()));
+        LOGGER.info("End");
+    }
+}

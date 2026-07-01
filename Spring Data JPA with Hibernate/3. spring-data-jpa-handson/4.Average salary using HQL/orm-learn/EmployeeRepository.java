9e270d5316b0e87de8814f8f7eec39535c6086b0
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 23:29:23 +0530

    Create EmployeeRepository.java

---
+++ Spring Data JPA with Hibernate/3. spring-data-jpa-handson/4.Average salary using HQL/orm-learn/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.cognizant.ormlearn.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cognizant.ormlearn.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e FROM Employee e left join fetch e.department left join fetch e.skillList WHERE e.permanent = true")
+    List<Employee> getAllPermanentEmployees();
+
+    @Query("SELECT AVG(e.salary) FROM Employee e WHERE e.department.id = :id")
+    double getAverageSalary(@Param("id") int id);
+}

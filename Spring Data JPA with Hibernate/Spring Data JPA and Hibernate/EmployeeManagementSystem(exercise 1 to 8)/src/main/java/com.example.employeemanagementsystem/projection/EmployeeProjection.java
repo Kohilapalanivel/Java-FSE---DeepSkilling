86ee8155537a3ed6fe8c51a668612d20738da20d
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-02 01:11:53 +0530

    Create EmployeeProjection.java

---
+++ Spring Data JPA with Hibernate/Spring Data JPA and Hibernate/EmployeeManagementSystem(exercise 1 to 8)/src/main/java/com.example.employeemanagementsystem/projection/EmployeeProjection.java
@@ -0,0 +1,10 @@
+package com.example.employeemanagementsystem.projection;
+
+public interface EmployeeProjection {
+
+    Long getId();
+    String getName();
+    String getEmail();
+    Double getSalary();
+    String getDepartment_Name();
+}

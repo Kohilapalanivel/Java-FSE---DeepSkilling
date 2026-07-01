7faedcf65829aa5d0119846f2dd0898827ae7172
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 09:41:13 +0530

    Create StockRepository.java

---
+++ Spring Data JPA with Hibernate/2. spring-data-jpa-handson/2.Query Methods In Stock Table/StockRepository.java
@@ -0,0 +1,12 @@
+package com.cognizant.ormlearn.repository;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cognizant.ormlearn.model.Stock;
+public interface StockRepository extends JpaRepository<Stock, Integer> 
+{
+    List<Stock> findByCodeAndDateBetween(String code,LocalDate startDate,LocalDate endDate);
+    List<Stock> findByCodeAndCloseGreaterThan(String code,double close);
+    List<Stock> findTop3ByOrderByVolumeDesc();
+    List<Stock> findTop3ByCodeOrderByCloseAsc(String code);
+}

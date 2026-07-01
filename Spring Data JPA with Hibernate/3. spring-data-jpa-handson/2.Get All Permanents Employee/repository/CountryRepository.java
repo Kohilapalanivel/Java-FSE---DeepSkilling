488710d947f6de5d01885745e0828d1a0e4224e2
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-01 19:58:32 +0530

    Refactor CountryRepository interface formatting

---
+++ Spring Data JPA with Hibernate/3. spring-data-jpa-handson/2.Get All Permanents Employee/repository/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.cognizant.ormlearn.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cognizant.ormlearn.model.Country;
+public interface CountryRepository extends JpaRepository<Country, String>
+{
+    Country findByCode(String code);
+    List<Country> findByNameContainingIgnoreCase(String text);
+    List<Country> findByNameContainingIgnoreCaseOrderByNameAsc(String text);
+    List<Country> findByNameStartingWithIgnoreCase(String alphabet);
+}

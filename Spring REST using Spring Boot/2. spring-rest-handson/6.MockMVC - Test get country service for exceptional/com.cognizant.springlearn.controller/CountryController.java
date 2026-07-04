7bf03126b161028868f1ab079da8ee987b4f54b6
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-04 22:12:11 +0530

    Create CountryController.java

---
+++ Spring REST using Spring Boot/2. spring-rest-handson/6.MockMVC - Test get country service for exceptional/com.cognizant.springlearn.controller/CountryController.java
@@ -0,0 +1,46 @@
+package com.cognizant.springlearn.controller;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+@RestController
+public class CountryController 
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+    @Autowired
+    private CountryService countryService;
+    // MockMVC Test
+    @RequestMapping("/country")
+    public Country getCountryIndia() throws CountryNotFoundException 
+    {
+        LOGGER.info("START");
+        Country country = countryService.getCountry("IN");
+        LOGGER.info("END");
+        return country;
+    }
+    // Get all countries
+    @GetMapping("/countries")
+    public ArrayList<Country> getAllCountries() {
+        LOGGER.info("START");
+        ArrayList<Country> countryList = countryService.getAllCountries();
+        LOGGER.info("END");
+        return countryList;
+    }
+
+    // Get country by code
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) throws CountryNotFoundException {
+        LOGGER.info("START");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END");
+        return country;
+    }
+}

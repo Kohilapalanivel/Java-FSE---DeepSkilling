6de091e55a125baa034ef90a51984849e6c11a31
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-06 10:49:40 +0530

    Create CountryController.java

---
+++ Spring REST using Spring Boot/4. spring-rest-handson/3.Read country data as a bean in RESTful Web Service/com.cognizant.springlearn/controller/CountryController.java
@@ -0,0 +1,107 @@
+/*
+package com.cognizant.springlearn.controller;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    // GET All Countries
+    @GetMapping
+    public ArrayList<Country> getAllCountries() {
+        LOGGER.info("START");
+        ArrayList<Country> countryList = countryService.getAllCountries();
+        LOGGER.info("END");
+        return countryList;
+    }
+
+    // GET Country by Code
+    @GetMapping("/{code}")
+    public Country getCountry(@PathVariable String code) throws CountryNotFoundException {
+        LOGGER.info("START");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END");
+        return country;
+    }
+
+    // POST Country (Handson 2)
+    @PostMapping
+    public void addCountry() {
+        LOGGER.info("START");
+    }
+
+    // PUT Country
+    @PutMapping
+    public void updateCountry(@RequestBody Country country) {
+        LOGGER.info("START");
+    }
+
+    // DELETE Country
+    @DeleteMapping("/{code}")
+    public void deleteCountry(@PathVariable String code) {
+        LOGGER.info("START");
+    }
+}
+*/
+
+
+
+
+package com.cognizant.springlearn.controller;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
+@RestController
+@RequestMapping("/countries")
+public class CountryController
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+    @Autowired
+    private CountryService countryService;
+    @GetMapping
+    public List<Country> getAllCountries() 
+    {
+        LOGGER.info("START");
+        List<Country> list = countryService.getAllCountries();
+        LOGGER.info("END");
+        return list;
+    }
+    @PostMapping
+    public Country addCountry(@RequestBody Country country) 
+    {
+        LOGGER.info("START");
+        LOGGER.info("Country : {}", country);
+        return country;
+    }
+}

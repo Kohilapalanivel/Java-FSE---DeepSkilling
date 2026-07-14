6bf7242fb81a59daecfe65f33bd1200ded60d44e
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-14 18:49:33 +0530

    Create Product.java

---
+++ Microservices/1. Microservices using Spring Boot 3 exercises/2. Inventory Management System with Service Discovery/product-service/main/java/com.cognizant.productservice/entity/Product.java
@@ -0,0 +1,50 @@
+package com.cognizant.productservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private int stock;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, int stock) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}

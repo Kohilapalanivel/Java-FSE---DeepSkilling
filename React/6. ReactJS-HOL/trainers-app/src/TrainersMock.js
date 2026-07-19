2ff3992d2f3e9b48055bd5538eda257823adb921
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-19 15:14:01 +0530

    Add files via upload

---
+++ React/6. ReactJS-HOL/trainers-app/src/TrainersMock.js
@@ -0,0 +1,34 @@
+import Trainer from "./trainer";
+
+const trainers = [
+
+new Trainer(
+1,
+"John",
+"john@gmail.com",
+"9876543210",
+"Java",
+"Core Java, Spring Boot"
+),
+
+new Trainer(
+2,
+"Emma",
+"emma@gmail.com",
+"9876543211",
+"React",
+"React, JavaScript"
+),
+
+new Trainer(
+3,
+"David",
+"david@gmail.com",
+"9876543212",
+".NET",
+"C#, ASP.NET"
+)
+
+];
+
+export default trainers;

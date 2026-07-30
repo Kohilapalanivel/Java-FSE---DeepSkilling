7b528c37dd6fa8f70615dbd5fc4cfa1fa606b62e
Kohilapalanivel <kohilapalanivel810@gmail.com>
2026-07-30 23:00:56 +0530

    Add files via upload

---
+++ ANGULAR/student-course-portal/src/app/components/notification/notification.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotificationService } from '../../services/notification';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './notification.html',
+  styleUrl: './notification.css',
+
+  // Component-level Provider
+  providers: [NotificationService]
+})
+export class Notification {
+
+  constructor(
+    public notificationService: NotificationService
+  ) {}
+
+}
